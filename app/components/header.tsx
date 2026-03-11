f65e8e41ba34484ec269330c978ffce240f32668
sereitepy <106307116+sereitepy@users.noreply.github.com>
2026-03-11 16:36:30 +0700

    Initial set up
    not done yet, switching computers

---
+++ app/components/header.tsx
@@ -0,0 +1,10 @@
+import { ThemeTogglerButton } from "@/components/animate-ui/components/buttons/theme-toggler";
+
+export default function Header() {
+  return (
+    <div>
+      <p>Logo</p>
+      <ThemeTogglerButton />
+    </div>
+  )
+}
